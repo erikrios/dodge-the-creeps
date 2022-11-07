025451a782682d7927ee4c73ef90d715cfa23865
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-11-07 09:15:34 +0700

    Heads up display and finishing up

--- Main.gd
+++ Main.gd
@@ -11,12 +11,18 @@ func _ready():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 	
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
@@ -24,6 +30,7 @@ func _on_StartTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_MobTimer_timeout():
 	# Create a new instance of the Mob scene.
